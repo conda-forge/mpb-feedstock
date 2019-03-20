5e5a419324f89ed721119d5a4af79df1eac3dfb2
Chris Hogan <c096h800@gmail.com>
2019-03-20 15:51:03 -0500

    Add MPI variant

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if [[ ! -z "$mpi" && "$mpi" != "nompi" ]]; then
+    # We don't actually compile parallel MPB here (--enable-parallel) because
+    # the Python MPB interface in Pymeep isn't compatible with parallel MPB.
+    # However, since the parallel Python interface uses parallel HDF5, we
+    # need to build MPB against parallel HDF5, which just requires the MPI
+    # compiler wrappers.
+    export CC=mpicc
+    export CXX=mpicxx
+fi
+
 ./configure --prefix="${PREFIX}" --enable-shared --with-libctl=no --with-hermitian-eps
 
 make
